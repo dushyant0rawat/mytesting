6a4ddfc7bf115e349bf2be97320738f69de39fe5
User <dushyant0rawat@gmail.com>
2022-08-30 15:32:12 -0400

    navigation

--- app/src/main/java/com/example/mytesting/MainActivity.kt
+++ app/src/main/java/com/example/mytesting/MainActivity.kt
@@ -5,8 +5,10 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -15,6 +17,9 @@ import androidx.compose.ui.Modifier
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.mytesting.ui.theme.MyTestingTheme
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.MultiplePermissionsState
@@ -22,8 +27,6 @@ import com.google.accompanist.permissions.rememberMultiplePermissionsState
 import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
-    @SuppressLint("CoroutineCreationDuringComposition")
-    @OptIn(ExperimentalPermissionsApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -33,18 +36,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    val permissionsState = rememberMultiplePermissionsState(
-                        permissions = listOf(
-                            Manifest.permission.RECORD_AUDIO,
-                            Manifest.permission.CAMERA
-                        )
-                    )
-                    lifecycleScope.launch {
-                        repeatOnLifecycle(Lifecycle.State.STARTED){
-                            permissionsState.launchMultiplePermissionRequest()
-                        }
-                    }
-                    GetContentExample(permissionsState)
+                    GetContentExample()
                 }
             }
         }
@@ -53,35 +45,85 @@ class MainActivity : ComponentActivity() {
 
     }
 
-@OptIn(ExperimentalPermissionsApi::class)
 @Composable
-fun GetContentExample(state: MultiplePermissionsState) {
+fun GetContentExample() {
+    val navController = rememberNavController()
+    NavHost(
+    navController = navController,
+        startDestination = "home"
+    ){
+        composable(route ="home"){
+            HomeScreen {
+                navController.navigate("page1")
+            }
+        }
+        composable(route ="page1"){
+            Page1Screen {
+                navController.navigate("page2")
+            }
+        }
+        composable(route ="page2"){
+            Page2Screen {
+                navController.navigate("page3")
+            }
+        }
+        composable(route ="page3"){
+            Page3Screen{
+                navController.navigate("home")
+            }
+        }
+    }
+
+
+}
+
+@Composable
+fun HomeScreen(
+    onNavigateToFriends: () -> Unit
+) {
 
     Column(
-        modifier= Modifier
-            .fillMaxSize()
+        verticalArrangement = Arrangement.SpaceBetween,
+        modifier = Modifier.fillMaxSize()
     ){
-        state.permissions.forEach { perm->
-            when(perm.permission){
-                Manifest.permission.CAMERA -> {
-                    when {
-                        perm.hasPermission -> Text("Camera permission accepted")
-                        perm.permissionRequested -> Text("Camera permission requested before")
-                        else -> Text("Camera permission denied permanently")
-                    }
-                }
-                Manifest.permission.RECORD_AUDIO ->{
-                    when {
-                    perm.hasPermission -> Text("recrod audio permission accepted")
-                    perm.permissionRequested -> Text("record audio permission requested before")
-                    else -> {
-                        Text("record audio permission denied permanently")
-                    }
-                    }
-
-                }
-            }
+       Text("this is home screen")
+       Text("this is home screen")
+       Text("this is home screen")
+       Text("this is home screen")
+       Text("this is home screen")
+       Text("this is home screen")
+       Text("this is home screen")
+       Text("this is home screen")
+       Text("this is home screen")
+        Button(onClick = onNavigateToFriends) {
+            Text(text = "take to page 1")
         }
     }
+}
 
+@Composable
+fun Page1Screen(
+    onNavigateToFriends: () -> Unit
+) {
+    Button(onClick = onNavigateToFriends) {
+        Text(text = "page1 screen, from here to page 2")
+    }
+}
+
+@Composable
+fun Page2Screen(
+    onNavigateToFriends: () -> Unit
+) {
+    Button(onClick = onNavigateToFriends) {
+        Text(text = "page2 screen, from here to page 3")
+    }
+}
+
+@Composable
+fun Page3Screen(
+    onNavigateToFriends: () -> Unit
+) {
+    Button(onClick = onNavigateToFriends) {
+        Text(text = "page3 screen, from here to home")
+    }
 }
