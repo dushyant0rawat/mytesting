7eb3ef98e98896ddf5dd78e158005410649870a9
User <dushyant0rawat@gmail.com>
2022-09-27 09:33:56 -0400

    param passing

--- app/src/main/java/com/example/mytesting/MainActivity.kt
+++ app/src/main/java/com/example/mytesting/MainActivity.kt
@@ -2,15 +2,21 @@ package com.example.mytesting
 
 import android.Manifest
 import android.annotation.SuppressLint
+import android.graphics.Paint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
@@ -56,11 +62,11 @@ fun GetContentExample() {
     ){
         composable(route ="home"){
             HomeScreen { name,age->
-                navController.navigate("page1" + "/$name/$age")
+                navController.navigate("page1/$name/$age")
             }
         }
         composable(
-            route ="page1" + "/{name}/{age}",
+            route ="page1"  + "/{name}/{age}",
             arguments = listOf(
                 navArgument("name"){
                     type = NavType.StringType
@@ -71,7 +77,9 @@ fun GetContentExample() {
                     defaultValue= -1
                 }
             )){
-            Page1Screen(name= it.arguments?.getString("name")!!,age=it.arguments?.getInt("age")!! ) {
+            Page1Screen(
+                name= it.arguments?.getString("name")!!,
+                age=it.arguments?.getInt("age")!! ) {
                 navController.navigate("page2")
             }
         }
@@ -97,23 +105,29 @@ fun HomeScreen(
 
     Column(
         verticalArrangement = Arrangement.SpaceBetween,
+        horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier.fillMaxSize()
     ){
        Text("this is home screen",
-       fontSize = 42.sp)
+       fontSize = 42.sp,
+       )
         var name by remember {
             mutableStateOf("")
         }
         var age by remember {
             mutableStateOf("")
         }
-       TextField(value = name, onValueChange = {
+       TextField(value = name,
+           onValueChange = {
            name = it
        })
-        TextField(value = age, onValueChange = {
+        TextField(value = age,
+            onValueChange = {
            age = it
-       })
-        Button(onClick = {onNavigate(name,age.toInt())}) {
+       },
+            keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number)
+        )
+        Button(onClick = {onNavigate(name.ifEmpty { " " },(age.ifEmpty { "0" }).toInt())}) {
             Text(text = "take to page 1")
         }
     }
@@ -125,10 +139,19 @@ fun Page1Screen(
     age: Int,
     onNavigate: () -> Unit
 ) {
-    Button(onClick = onNavigate) {
-        Text(text = "page1 screen, from here to page 2" +
-                "argument are $name and $age")
-    }
+        Column(
+            modifier= Modifier
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.SpaceEvenly,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+            Text("name is $name")
+            Text("age is $age")
+            Button(onClick = onNavigate) {
+                Text(text = "page1 screen, from here to page 2",
+                textAlign = TextAlign.Center)
+            }
+        }
 }
 
 @Composable
