34c126992408b9c241e5d9ee1b717cd76d3ef282
User <dushyant0rawat@gmail.com>
2022-10-03 09:12:15 -0400

    rationale alert dialog

--- app/src/main/java/com/example/mytesting/MainActivity.kt
+++ app/src/main/java/com/example/mytesting/MainActivity.kt
@@ -5,13 +5,13 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
@@ -76,7 +76,10 @@ fun GetContentExample(state: MultiplePermissionsState) {
                     when {
                         perm.hasPermission -> Text("Camera permission granted")
                         perm.permissionRequested -> Text("Camera permission requested before")
-                        perm.shouldShowRationale -> Text("Camera rationale true")
+                        perm.shouldShowRationale -> {
+                        Text("Camera rationale true")
+                            ShowRationale()
+                        }
                         else-> Text("Camera permission denied permanently")
                     }
                 }
@@ -94,4 +97,42 @@ fun GetContentExample(state: MultiplePermissionsState) {
         }
     }
 
+}
+
+@Composable
+fun ShowRationale() {
+
+    val openDialog = remember { mutableStateOf(true)}
+
+    if (openDialog.value)
+        AlertDialog(
+        onDismissRequest = {},
+        title = {
+            Text(text = "Permission Rationale")
+        },
+        text = {
+            Text(
+                "Audio Permission is required to record"
+            )
+        },
+        buttons = {
+            Row(
+                modifier = Modifier.padding(all = 8.dp),
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Button(
+                    modifier = Modifier.fillMaxWidth(),
+                    onClick = { openDialog.value=false }
+                ) {
+                    Text("OK")
+                }
+                Button(
+                    modifier = Modifier.fillMaxWidth(),
+                    onClick = {  openDialog.value=false}
+                ) {
+                    Text("Cancel")
+                }
+            }
+        }
+    )
 }
